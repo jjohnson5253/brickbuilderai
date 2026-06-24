59d5768d904aae89ef3bf56a536979de5188e7f9
Jake Johnson <jakejohnson3700@gmail.com>
2026-06-24 02:12:08 -0500

    hide announcement banner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <BrowserRouter>
       {/* <ProtectedRoute> */}
-        <AnnouncementBanner />
+        {/* <AnnouncementBanner /> */}
         <Routes>
           {/* Public */}
           <Route path="/" element={<LandingPage />} />
