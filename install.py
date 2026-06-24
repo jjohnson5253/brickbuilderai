462192cc1694b2c2f21e7846b742ba8728390e5e
Jake Johnson <jakejohnson3700@gmail.com>
2026-06-24 01:07:06 -0500

    try to ignore interrupt in install

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
+import signal
 import subprocess
 
+signal.signal(signal.SIGINT, signal.SIG_IGN)
 subprocess.run(["uv", "sync", "--no-progress"], cwd="backend", check=True)
 subprocess.run(["npm", "install"], cwd="frontend", check=True)
