b9d88585b64f5ed33ea64b854dc6cdb9a8e5f19f
Jake Johnson <jakejohnson3700@gmail.com>
2026-06-24 01:03:22 -0500

    try to fix keyboard interrupt on install

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.run(["uv", "sync"], cwd="backend", check=True)
+subprocess.run(["uv", "sync", "--no-progress"], cwd="backend", check=True)
 subprocess.run(["npm", "install"], cwd="frontend", check=True)
